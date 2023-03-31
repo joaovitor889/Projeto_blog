2c9e67d016bca3d7b881ed4dbb4c2d8378c4738d
unknown <joaovitor.olveira16@gmail.com>
2023-03-31 15:42:14 -0300

    Cadastro  de  categoria e  slugify

--- categories/CateforiesController.js
+++ categories/CateforiesController.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const Category  =  require("./Category");
+const slugify = require("slugify");
 
-router.get("/categories", (req, res)=>{
-    res.send("Rota de  categorias")
+router.get("/admin/categories/new", (req, res) => {
+    res.render("admin/categories/new");
 });
 
-router.get("/adimin/categories/new", (req, res) =>{
-    res.send("ROTA PARA  CRIAR UMA NOVA CATEGORIA")
-})
+router.post("/categories/save", (req, res) =>{
+    var title = req.body.title;
+    if(title != undefined){
+
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => { 
+           res.redirect("/"); 
+        })
+
+    }else{
+        res.redirect("/admin/categories/new");
+    }
+});
 
 module.exports =router;
