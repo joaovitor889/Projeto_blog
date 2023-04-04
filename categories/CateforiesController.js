30f3d1d9ce9cdcc5c97eb3cb5dae78d2d3cac81a
unknown <joaovitor.olveira16@gmail.com>
2023-04-04 11:05:30 -0300

    Botão de edição das categorias

--- categories/CateforiesController.js
+++ categories/CateforiesController.js
@@ -15,7 +15,7 @@ router.post("/categories/save", (req, res) =>{
             title: title,
             slug: slugify(title)
         }).then(() => { 
-           res.redirect("/"); 
+           res.redirect("/admin/categories"); 
         })
 
     }else{
@@ -48,5 +48,35 @@ router.post("/categories/delete",  (req, res) => {
         }
     }
 });
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+    Category.findByPk(id).then(category => {
+        if(category != undefined){
+            res.render("admin/categories/edit",{category: category});
+        }else{
+            res.redirect("/admin/categories")
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    })
+});
+
+router.post("/categories/update", (req, res) => {
+    var id =  req.body.id;
+    var title = req.body.title;
+
+    Category.update({title: title, slug: slugify(title)},{
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    })
+})
 
 module.exports = router;
