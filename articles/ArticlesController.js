1cfdfaf7d6fc3573b7c9047451ff165da71a4c37
JoaoReis <joaovitor.olveira16@gmail.com>
2023-07-26 21:41:39 -0300

    Logica de paginação

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -90,5 +90,35 @@ router.post("/articles/update", (req, res) => {
         res.redirect("/");
     })
 
-})
+});
+
+router.get("/articles/page/:num",(req, res) => {
+    var page = req.params.num;
+    var offset = 0;
+
+    if(isNaN(page) || page == 1){
+        offset = 0;
+    }else{
+        offset = parseInt(page) * 4;
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: 1
+    }).then(articles=> {
+
+        var next;
+        if(offset + 4 >= articles.count){
+            next = false;
+        }else{
+            next = true;
+        }
+
+        var result = {
+            next: next,
+            articles : articles
+        }
+        res.json(result);
+    });
+});
 module.exports = router;
